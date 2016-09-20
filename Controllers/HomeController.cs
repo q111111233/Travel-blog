0aa88f25f0fa9ff637e6033288b06106674aecf7
Bo Zhao and Shradha Pulla <bo+shradha@>
2016-09-20 11:07:49 -0700

    Create log in & log out functionality for user

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,9 +13,11 @@ namespace TravelBlog.Controllers
     {
         private readonly ApplicationDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
-        public HomeController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public HomeController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
             _db = db;
         }
         public IActionResult Index()
@@ -41,6 +43,32 @@ namespace TravelBlog.Controllers
             {
                 return View();
             }
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult signInResult = await _signInManager.PasswordSignInAsync(model.Username, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (signInResult.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        //Log Out User
+        [HttpPost]
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
         }
     }
 }
